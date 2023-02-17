2c2299b72543406bc24717f12f28d5e4e9589dc0
Jasser Blanco <yaser.keny@gmail.com>
2023-02-17 15:55:36 -0500

    Api stripe Finished

---
+++ Creditos/assets/stripe-keys.js
@@ -0,0 +1,6 @@
+export default {
+  public:
+    "pk_test_51LGllkFVtJKu5C8LTrVJy9cQISBGqM0d6Gr13RBDjTAoUe8D46cPheuM80mV3O0ICGSPQCWRwEyNbgcAPFffoSpf00jH9Ckksm",
+  secret:
+    "sk_test_51LGllkFVtJKu5C8LrBXgcFkboxS60SeefnzHtI35H9AUufghujWkLzziPcwNgVl9HsqXj2KRgF1W7Zi8UvRzKT9I00FVDICJHB",
+};
--- Creditos/main.js
+++ Creditos/main.js
@@ -6,6 +6,20 @@ var app = new Vue({
     productsApi: [],
     productsApiID: [],
     showLoader: false,
+    paymentCard: [
+      {
+        bank: "Visa",
+        numbers: [4242424242424242, 4000056655665556],
+      },
+
+      {
+        bank: "Mastercard",
+        numbers: [
+          5555555555554444, 2223003122003222, 5200828282828210,
+          5105105105105100,
+        ],
+      },
+    ],
   },
 
   methods: {
@@ -19,6 +33,12 @@ var app = new Vue({
       let products;
       let IdProducs = [];
       let apiProducs = [];
+      const formatter = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 0,
+      });
+
       this.showLoader = true;
       Promise.all([
         fetch("https://api.stripe.com/v1/products", fetchOptions),
@@ -42,15 +62,22 @@ var app = new Vue({
               apiProducs.push(productData[0]);
               this.productsApi = [...apiProducs.slice(0, 4)];
               let newArray = this.productsApiID.map((el) => el);
+              let newPrices = prices.map((el) => el.unit_amount_decimal);
+              newPrices = [...newPrices.slice(0, 4)];
+              newPrices = newPrices.map((el) =>
+                formatter.format(`${el.slice(0, -2)}`)
+              );
+              // console.log(newPrices);
               this.productsApi = this.productsApi.map((product, index) => {
                 return {
                   ...product,
                   dataPrice: newArray[index],
+                  price: newPrices[index],
                 };
               });
             });
             // console.log(this.productsApiID);
-            console.log(this.productsApi);
+            // console.log(this.productsApi);
             this.showLoader = false;
           })
           .catch((err) => {
@@ -61,6 +88,28 @@ var app = new Vue({
           })
       );
     },
+
+    buyCoins(e) {
+      let price =
+        e.target.parentElement.parentElement.getAttribute("data-price");
+      // console.log(price);
+      this.showLoader = true;
+      Stripe(STRIPE.public)
+        .redirectToCheckout({
+          lineItems: [{ price, quantity: 1 }],
+          mode: "payment",
+          successUrl:
+            "http://127.0.0.1:5500/Creditos/assets/stripe-succes.html",
+          cancelUrl: "http://127.0.0.1:5500/Creditos/assets/stripe-cancel.html",
+        })
+        .then((res) => {
+          alert(res);
+          console.log(res.error);
+          $tacos.insertAdjacentHTML("afterend", res.error.message);
+          return result.json();
+        });
+      this.showLoader = true;
+    },
   },
 
   created() {
@@ -68,3 +117,22 @@ var app = new Vue({
   },
   mouted() {},
 });
+
+// d.addEventListener("click", (e) => {
+//   if (e.target.matches(".taco *")) {
+//     let price = e.target.parentElement.dataset.price;
+//     // console.log(price);
+//     Stripe(STRIPE_KEYS.public)
+//       .redirectToCheckout({
+//         lineItems: [{ price, quantity: 1 }],
+//         mode: "subscription",
+//         successUrl: "http://127.0.0.1:5500/ajax-ejercicios/assets/succes.html",
+//         cancelUrl: "http://127.0.0.1:5500/ajax-ejercicios/assets/cancel.html",
+//       })
+//       .then((res) => {
+//         alert(res);
+//         console.log(res.error);
+//         $tacos.insertAdjacentHTML("afterend", res.error.message);
+//       });
+//   }
+// });
