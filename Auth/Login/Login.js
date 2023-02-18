e2889ce2177a817dbfa8788bbfb5a582fdf6a7e2
sebastian-crow <sebastianberiio@gmail.com>
2023-02-18 16:16:49 -0500

    Merging progress

--- Auth/Login/Login.js
+++
@@ -1,6 +0,0 @@
-export const Login = {
-  template: "<h1>This is Login Section</h1>",
-  created() {
-    console.log("Has been created");
-  },
-};
--- Auth/Register/Register.js
+++
@@ -1,6 +0,0 @@
-export const Register = {
-  template: "<h1>This is Register Section</h1>",
-  created() {
-    console.log("Has been created");
-  },
-};
